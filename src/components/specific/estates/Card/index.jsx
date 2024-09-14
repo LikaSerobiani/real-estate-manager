4c92ea7fedfcb20b6c708e8f226a75df3a081462
Lika <lika.likuna2008@gmail.com>
2024-09-14 21:08:31 +0400

    feat: added card/icons/fonts and colors

--- src/components/specific/estates/Card/index.jsx
+++ src/components/specific/estates/Card/index.jsx
@@ -1,6 +1,85 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-export default function Card() {
-  return <div></div>;
+import LocationMarker from "../../../common/Icons/LocationMarker";
+import Bed from "../../../common/Icons/Bed";
+import Resize from "../../../common/Icons/Resize";
+import PostSign from "../../../common/Icons/PostSign";
+
+export default function Card({
+  image,
+  title,
+  price,
+  address,
+  zip_code,
+  area,
+  bedrooms,
+}) {
+  return (
+    <div
+      className="w-96 h-[455px] rounded-[14px] cursor-pointer"
+      style={{
+        boxShadow: "none",
+      }}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.boxShadow = "5px 5px 12px 0px #02152614";
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.boxShadow = "none";
+      }}
+    >
+      <div>
+        <img
+          src={image}
+          alt={title}
+          className="h-[307px] rounded-t-[14px] rounded-b-none"
+        />
+      </div>
+      <div
+        className="h-[148px] rounded-b-[14px] rounded-t-none p-[22px] px-[25px] flex flex-col gap-5 bg-white font-firago"
+        style={{
+          borderWidth: "0px 1px 1px 1px",
+          borderColor: "#DBDBDB",
+        }}
+      >
+        <div className="w-[334px] h-[60px] flex flex-col gap-[6px]">
+          <span className=" font-bold leading-[33.6px] text-[28px] text-[#021526] h-[34px]">
+            {price} ₾
+          </span>
+          <div className="flex gap-1 h-5">
+            <LocationMarker />
+            <span className="text-[#021526] text-opacity-70  text-[16px] font-normal leading-[19.2px]">
+              {address}
+            </span>
+          </div>
+        </div>
+        <div className="w-[214px] h-6 flex gap-[32px] text-[#021526] text-opacity-70 items-center">
+          <div className="w-[37px] h-[24px] flex gap-[5px] items-center">
+            <Bed />
+            <span className="w-2 h-[19px] font-normal text-[16px] leading-[19.2px]">
+              {bedrooms}
+            </span>
+          </div>
+          <div className="w-[59px] h-[19px] flex items-center gap-[5px]">
+            <Resize />
+            <div className="w-[36px] flex gap-[1px]">
+              <span className="w-[30px] font-normal text-[15px] leading-[19.2px]">
+                {area} მ
+              </span>
+              <span className="w-[5px] h-3 text-[10px] leading-3 font-normal">
+                2
+              </span>
+            </div>
+          </div>
+          <div className="w-[54px] h-[19px] flex items-center gap-[5px]">
+            <PostSign />
+            <span className="leading-[19.2px] text-[16px] font-normal">
+              {zip_code}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        firago: ["Firago", "sans-serif"],
+      },
+      colors: {
+        primary: "#F93B1D",
+        secondary: "#021526",
+        white: "#FFFFFF",
+        "primary-opacity": "#02152680",
+        lightGray: "#808A93",
+        darkGray: "#676E76",
+        border: "#DBDBDB",
+      },
+    },
   },
   plugins: [],
 };

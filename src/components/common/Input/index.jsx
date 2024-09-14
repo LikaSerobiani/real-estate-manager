a525d7edba12b97880cb9cb7a7db99c08ae48401
Lika <lika.likuna2008@gmail.com>
2024-09-14 22:19:31 +0400

    feat: added input component

--- src/components/common/Input/index.jsx
+++ src/components/common/Input/index.jsx
@@ -1,6 +1,23 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-export default function Input() {
-  return <div></div>;
+export default function Input({ label, type, id, value, onChange }) {
+  return (
+    <div className="w-[384px] h-[64px] flex flex-col gap-[5px]">
+      <label
+        htmlFor={id || "inputField"}
+        className="font-firago font-medium text-[14px] leading-[16.8px] text-secondary h-[17px]"
+      >
+        {label}
+      </label>
+      <input
+        type={type || "text"}
+        id={id}
+        value={value}
+        className="h-[42px] rounded-[6px] border p-[10px] border-lightGray"
+        onChange={onChange}
+      />
+    </div>
+  );
 }

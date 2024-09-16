e9957f02b354f13cf85d4620587d3fcaf2ce81a1
Lika <lika.likuna2008@gmail.com>
2024-09-17 01:18:05 +0400

    feat: added file uploader component

---
+++ src/components/common/FileUploader/index.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import PlusCircle from "../Icons/PlusCircle";
+import TrashCircle from "../Icons/TrashCircle";
+
+const FileUploader = ({ onFileChange }) => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      setFile(URL.createObjectURL(selectedFile));
+      if (onFileChange) {
+        onFileChange(selectedFile);
+      }
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setFile(null);
+    if (onFileChange) {
+      onFileChange(null);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-[5px]">
+      <label className="font-firago font-bold text-[14px] leading-[16.8px] text-secondary">
+        ატვირთეთ ფოტო*
+      </label>
+      <div className="relative w-[799px] h-[120px] rounded-[8px] border border-dashed border-[#2D3648] bg-white">
+        <input
+          type="file"
+          id="fileInput"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+        <label
+          htmlFor="fileInput"
+          className="absolute inset-0 flex items-center justify-center cursor-pointer"
+        >
+          {!file ? (
+            <PlusCircle className="text-gray-500 text-3xl" />
+          ) : (
+            <img
+              src={file}
+              alt="Uploaded"
+              className="absolute w-[92px] h-[82px] rounded-[4px] object-cover"
+            />
+          )}
+        </label>
+        {file && (
+          <div
+            className="absolute top-[83px] left-[429px] cursor-pointer"
+            onClick={handleRemoveImage}
+          >
+            <TrashCircle />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+FileUploader.propTypes = {
+  onFileChange: PropTypes.func.isRequired,
+  className: PropTypes.string,
+};
+
+export default FileUploader;

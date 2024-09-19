54cd90f1aec30f8977ead207bdeb71f47c6ddde4
Lika <lika.likuna2008@gmail.com>
2024-09-19 18:45:32 +0400

    feat: added estate listin on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import EstateListing from "../pages/estates/EstateListing";
+import useEstateStore from "../stores/useEstateStore";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { estates, fetchEstates } = useEstateStore();
+
+  useEffect(() => {
+    fetchEstates();
+  }, [fetchEstates]);
+
+  return (
+    <div>
+      <EstateListing estates={estates} />
+    </div>
+  );
 }

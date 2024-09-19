3b18496343018f155d1597458985b9bbe8abaf06
Lika <lika.likuna2008@gmail.com>
2024-09-20 01:45:16 +0400

    feat: added new icon

---
+++ src/components/common/Icons/ExclamationMark.jsx
@@ -0,0 +1,23 @@
+export default function ExclamationMark({ color }) {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="13px"
+        height="12px"
+        viewBox="0 0 64 64"
+        aria-hidden="true"
+        role="img"
+        preserveAspectRatio="xMidYMid meet"
+        fill={color}
+      >
+        <g id="SVGRepo_iconCarrier">
+          <path d="M36.989 42.439H27.01L23 2h18z" fill={color} />
+
+          <ellipse cx="31.999" cy="54.354" rx="7.663" ry="7.646" fill={color} />
+        </g>
+      </svg>
+    </div>
+  );
+}

7d0a938e1ce71b22931521eedb88893743684999
Lika <lika.likuna2008@gmail.com>
2024-09-19 18:31:48 +0400

    feat: added estate listing

--- src/pages/estates/EstateListing.jsx
+++ src/pages/estates/EstateListing.jsx
@@ -1,3 +1,28 @@
-export default function EstateListing() {
-  return <div>EstateListing</div>;
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import Card from "../../components/specific/estates/Card";
+
+export default function EstateListing({ estates }) {
+  return (
+    <div>
+      <div className="flex flex-wrap gap-5">
+        {estates.map((estate) => (
+          <div key={estate.id}>
+            <Card
+              id={estate.id}
+              image={estate.image}
+              title={estate.title}
+              price={estate.price}
+              address={estate.address}
+              zip_code={estate.zip_code}
+              area={estate.area}
+              bedrooms={estate.bedrooms}
+              is_rental={estate.is_rental}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }

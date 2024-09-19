fc23d9a6242413f848c41ba6f36f2d779f4fdbad
Lika <lika.likuna2008@gmail.com>
2024-09-19 14:15:15 +0400

    feat: added loading component

---
+++ src/components/common/Loading/index.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="border-4 border-solid border-opacity-10 border-gray-100 border-t-gray-700 rounded-full w-12 h-12 animate-spin"></div>
+    </div>
+  );
+}

4e803fa5c102667097cfb437ba4cec29e535b646
Lika <lika.likuna2008@gmail.com>
2024-09-21 23:14:32 +0400

    fix: path of groupedcheckbox

--- src/components/specific/filter/RegionFilter.jsx
+++ src/components/specific/filter/RegionFilter.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import GroupedCheckbox from "../../common/GroupedCheckbox";
+import GroupedCheckbox from "../../common/GroupedCheckBox";
 
 const RegionFilter = ({ setRegions, regions }) => {
   return (
